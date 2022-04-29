415e7a6fab172b4460a7d725006baf522e828c82
Oleksandra <90516748+OleksandraTurik@users.noreply.github.com>
2022-04-30 01:52:14 +0300

    add some styles

---
+++ script.js
@@ -0,0 +1,11 @@
+function focusin()
+{
+    const modalWindow = document.getElementById('modal-window-id');
+    modalWindow.classList.toggle('active')
+}
+
+function focusout()
+{
+    const modalWindow = document.getElementById('modal-window-id');
+    modalWindow.classList.toggle('active')
+}
